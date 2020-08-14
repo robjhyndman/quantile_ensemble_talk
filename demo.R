b324910c949f8abc3df27ff500a382e2f5bca097
Rob Hyndman <robjhyndman@gmail.com>
2020-08-14 17:19:08 +1000

    Updated demo code

--- demo.R
+++ demo.R
@@ -1,35 +1,40 @@
+library(tidyverse)
+library(lubridate)
 library(fable)
 
 cafe <- readRDS("cafe.rds")
 
 cafe %>%
+  autoplot(turnover) +
+  scale_y_log10()
+
+cafe %>%
   filter(year(date) <= 2018) %>%
   model(
     ETS = ETS(turnover),
-    ARIMA = ARIMA(turnover ~
-                    pdq(d=1) + PDQ(D=1)),
+    ARIMA = ARIMA(turnover ~ pdq(d = 1) + PDQ(D = 1)),
     SNAIVE = SNAIVE(turnover)
   ) %>%
   mutate(
-    COMB = (ETS+ARIMA)/2
+    COMB = (ETS + ARIMA) / 2
   ) %>%
   forecast(h = "1 year") %>%
   accuracy(
     data = cafe,
-    measures = list(crps=CRPS, rmse=RMSE)
+    measures = list(crps = CRPS, rmse = RMSE)
   ) ->
   crps
 
 sn_crps <- crps %>%
-  filter(.model=="SNAIVE") %>%
-  select(state,crps) %>%
+  filter(.model == "SNAIVE") %>%
+  select(state, crps) %>%
   rename(sn_crps = "crps")
 
 crps %>%
   filter(.model != "SNAIVE") %>%
-  left_join(sn_crps, by="state") %>%
+  left_join(sn_crps, by = "state") %>%
   mutate(
-    skill = 100*(sn_crps - crps)/sn_crps
+    skill = 100 * (sn_crps - crps) / sn_crps
   ) %>%
   group_by(.model) %>%
   summarise(
